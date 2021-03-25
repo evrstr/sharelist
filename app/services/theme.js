50bebe1b4827a922bf21c3196155446751deffc1
evrstr <429427989yiqi@gmail.com>
2021-03-25 15:35:20 +0800

    asdad

--- app/services/theme.js
+++ app/services/theme.js
@@ -37,8 +37,7 @@ const renderMiddleware = ({ dir }) => (ctx, next) => {
         custom_style: getConfig('custom_style'),
         custom_script: getConfig('custom_script'),
         custom_githubAddr: process.env.GITHUBADDR,
-        custom_BLOGADDR: process.env.BLOGADDR,
-        custom_BLOGADDR: process.env.BLOGNAME,
+        custom_COPYRIGHT: process.env.COPYRIGHT,
       }
     }
     return render(ctx, path.resolve(dir, options.theme, 'view', relPath + '.pug'), data)
